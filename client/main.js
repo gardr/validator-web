9e54bba7f1d3096dde0720f99fc92b78a55b4ac0
sverosak <sveinung.rosaker@gmail.com>
2013-11-20 14:07:49 +0100

    move validation init to form post

--- client/main.js
+++ client/main.js
@@ -2,27 +2,18 @@ var events = require('dom-events');
 var domready = require('domready');
 
 domready(function () {
-    
-    initValidate();
 
     initRender();
 });
 
-function initValidate(){
-    var button = document.getElementById('start-validation');
-    var input = document.getElementById('urlInput');
-
-    function handler(){
-        //console.log('start validation');
-        window.location = '/validate?url='+window.encodeURIComponent(input.value);
-    }
-
-    events.on(button, 'click', handler);
-}
-
 function initRender() {
     var button = document.getElementById('render');
     var input = document.getElementById('urlInput');
+
+    if (!button || !input) {
+        return;
+    }
+
     var manager = global.getManager({
         iframeUrl: '/preview/html/pasties/mobile.htm'
     });
@@ -30,6 +21,7 @@ function initRender() {
     var counter = 0;
 
     function handler(event) {
+        event.preventDefault();
         var name = 'example_' + (counter++);
         //console.log(name);
         manager.queue({
@@ -40,6 +32,7 @@ function initRender() {
         manager.render(name, function (err, res) {
             console.log('done', name, err, res);
         });
+        return false;
     }
 
     events.on(button, 'click', handler);
