bb66be4f3c1013d6457c8b4d6f329b3c29d66264
sverosak <sveinung.rosaker@gmail.com>
2013-11-26 09:40:49 +0100

    add css tests

--- test/unit/pasties.test.js
+++ test/unit/pasties.test.js
@@ -45,17 +45,142 @@ describe('Pasties hooks', function () {
 
 });
 
-var validator = require('../../lib/validator/css.js');
-describe('CSS validator', function () {
+var validator = require('../../lib/validator/pasties.js');
 
-    it.skip('should evaluate client total size and report', function(){});
-    it.skip('should evaluate responsive position', function(){});
-    it.skip('should evaluate css position', function(){});
+describe('Pasties validator', function () {
 
-    it('should call next', function (done) {
+    it('should evaluate and return errors', function (done) {
+        var harvest = {
+            'pastiesDom': {
+                banner: {
+                    name: 'DIV',
+                    clickHandler: '',
+                    found: true,
+                    css: {
+                        position: 'absolute',
+                        height: '222px',
+                        display: 'block',
+                        width: '980px'
+                    }
+                },
+                wrapper: {
+                    css: {
+                        position: 'relative',
+                        visability: ''
+                    }
+                }
+            }
+        };
+
+
+        var reporter = help.createReporter.call(this);
+
+        validator.validate(harvest, reporter, function(){
+            var result = reporter.getResult();
+
+            assert.equals(result.error.length, 4);
+            done();
+        });
+
+    });
+
+
+    function getValid(clickHandler){
+        return {
+            'pastiesDom': {
+                banner: {
+                    name: 'DIV',
+                    clickHandler: clickHandler,
+                    found: true,
+                    css: {
+                        position: 'static',
+                        height: '225px',
+                        display: 'block',
+                        width: '980px'
+                    }
+                },
+                wrapper: {
+                    css: {
+                        position: 'static',
+                        display: 'block',
+                        visability: ''
+                    }
+                }
+            }
+        };
+    }
+
+
+    it('should error on missing clickhandler', function(done){
+
+        var harvest = getValid();
+
+        var reporter = help.createReporter.call(this);
+
+        validator.validate(harvest, reporter, function(){
+            var result = reporter.getResult();
+
+            assert.equals(result.error.length, 1);
+            done();
+        });
+
+    });
+
+    it('should pass on valid clickhandler', function(done){
+
+        var harvest = getValid('function(){window.open(url, "new_window");}');
+
+        var reporter = help.createReporter.call(this);
+
+        validator.validate(harvest, reporter, function(){
+            var result = reporter.getResult();
+
+            assert.equals(result.error.length, 0);
+        });
+
+
+        harvest = getValid('window.open(url, "new_window")');
+
+        validator.validate(harvest, reporter, function(){
+            var result = reporter.getResult();
+
+            assert.equals(result.error.length, 0);
+            done();
+        });
+
+    });
+
+    it('should error on invalid ref', function(done){
+
+        var harvest = getValid('function(){open(url, "_blank");}');
+
+        var reporter = help.createReporter.call(this);
+
+        validator.validate(harvest, reporter, function(){
+            var result = reporter.getResult();
+
+            assert.equals(result.error.length, 1);
+            done();
+        });
+    });
+
+
+    it('should error on invalid window target', function(done){
+
+        var harvest = getValid('window.open(url, "_blank")');
+
+        var reporter = help.createReporter.call(this);
+
+        validator.validate(harvest, reporter, function(){
+            var result = reporter.getResult();
+
+            assert.equals(result.error.length, 1);
+            done();
+        });
+    });
+
+    it('should call next if missing data', function (done) {
         validator.validate(null, null, done);
     });
 
 });
-
-
