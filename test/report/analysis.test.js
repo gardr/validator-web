c02b70b4d91ba9a23c32dcc7e127b03e1aa155ff
sverosak <sveinung.rosaker@gmail.com>
2013-12-04 15:44:07 +0100

    add simple geolocation static test

---
+++ test/report/analysis.test.js
@@ -0,0 +1,56 @@
+var buster = require('buster-assertions');
+var assert = buster.assert;
+var refute = buster.refute;
+
+var validator = require('../../lib/report/validator/analysis.js');
+var help = require('../lib/validateHelpers.js');
+
+
+describe('Static analysis', function(){
+
+
+    it('should report on geolocation usage', function(done){
+
+
+        var harvested = {
+            frameScriptTags: ['var nav = navigator; nav.geolocation'],
+            frameScriptAttributes: ['window.open("url", "new_window")', 'navigator.geolocation;'],
+            rawFileData: {
+                "http://localhost:8000/fixtures/script1.js?": {
+                    "url": "http://localhost:8000/fixtures/script1.js?",
+                    "redirects": [],
+                    "contentType": "application/javascript",
+                    "bodyLength": 493,
+                    "compressed": true,
+                    "base64Content": new Buffer("var nav=navigator; nav.geolocation; nav['geolocation']").toString('base64'),
+                    "unzippedSize": 991,
+                    "aproxCompressionPossible": 498,
+                    "aproxCompressedSize": 493
+                },
+                "http://code.jquery.com/jquery-1.10.1.js": {
+                    "url": "http://code.jquery.com/jquery-1.10.1.js",
+                    "requestError": true,
+                    "error": {
+                        "code": "ENOTFOUND",
+                        "errno": "ENOTFOUND",
+                        "syscall": "getaddrinfo"
+                    }
+                }
+            }
+        };
+        var reporter = help.createReporter.call(this);
+
+        validator.validate(harvested, reporter, handler, {});
+
+        function handler(){
+
+            var result = reporter.getResult();
+
+            assert.equals(result.error.length, 4, 'should report on geolocation usage');
+
+            done();
+        }
+
+    });
+
+});
