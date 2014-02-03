8efb3827fa4809817977b5e7646c3c6fbed970d7
sverosak <sveinung.rosaker@gmail.com>
2014-02-03 13:41:00 +0100

    move templates and split up.

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ var log         = require('./lib/logger.js');
 var Hapi        = require('hapi');
 var config      = require('./lib/config.js');
 
-Hapi.joi.version('v2');
-
 // Create a server with a host and port
 var PORT = config.get('httpServerPort');
 var options = {
@@ -15,7 +13,8 @@ var options = {
         engines: {
             html: 'handlebars'
         },
-        partialsPath: 'templates',
+        layout: true,
+        partialsPath: 'templates/partials',
         isCached: !development
     },
     cache: {
@@ -26,6 +25,19 @@ var options = {
 
 var server = new Hapi.Server('0.0.0.0', PORT, options);
 
+server.state('session', {
+    ttl: 24 * 60 * 60 * 1000,
+    isSecure: false,
+    path: '/',
+    encoding: 'base64json'
+});
+
+// server.pack.allow({ ext: true }).require('yar', {
+//     cookieOptions: {
+//         password: 'password'
+//     }
+// }, function (err) { });
+
 server.route(require('./lib/routes/frontpage.js'));
 server.route(require('./lib/routes/userInput.js'));
 server.route(require('./lib/routes/screenshots.js'));
@@ -33,7 +45,7 @@ server.route(require('./lib/routes/serveZip.js'));
 server.route(require('./lib/routes/validate.js'));
 server.route(require('./lib/routes/result.js'));
 server.route(require('./lib/routes/status.js'));
-server.addRoutes(require('./lib/routes/static.js').routes);
+server.route(require('./lib/routes/static.js').routes);
 
 server.start();
 
