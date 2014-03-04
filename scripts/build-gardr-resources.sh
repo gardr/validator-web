5aac2d9e610a0c205451e637db081c9deba44d6e
sverosak <sveinung.rosaker@gmail.com>
2014-03-04 10:15:29 +0100

    fix buildscript not running in pararell, wait for mkdirp to be done

--- scripts/build-gardr-resources.sh
+++ scripts/build-gardr-resources.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p ./lib/report/resources/built &&
-./node_modules/.bin/browserify  ./lib/report/resources/gardr-iframe.js > ./lib/report/resources/built/gardr-iframe.js &
+./node_modules/.bin/browserify  ./lib/report/resources/gardr-iframe.js > ./lib/report/resources/built/gardr-iframe.js &&
 ./node_modules/.bin/browserify  ./lib/report/resources/gardr-manager.js > ./lib/report/resources/built/gardr-manager.js
