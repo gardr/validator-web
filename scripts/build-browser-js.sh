b58d13a3885818dcf7bbfbba0aec66536a5a9be2
sverosak <sveinung.rosaker@gmail.com>
2014-03-06 11:52:41 +0100

    up hoek and add echo after build browser js

--- scripts/build-browser-js.sh
+++ scripts/build-browser-js.sh
@@ -5,4 +5,5 @@ mkdir -p ./client/built &&
 ./node_modules/.bin/browserify client/js/main.js -x domready -x dom-events  -o client/built/main.js &&
 ./node_modules/.bin/uglifyjs client/built/main.js -mc --lint=false > client/built/main.min.js &
 ./node_modules/.bin/browserify client/js/editor.js -x domready -x dom-events -o client/built/editor.js &&
-./node_modules/.bin/uglifyjs client/built/editor.js -mc --lint=false > client/built/editor.min.js
+./node_modules/.bin/uglifyjs client/built/editor.js -mc --lint=false > client/built/editor.min.js &&
+echo "done"
