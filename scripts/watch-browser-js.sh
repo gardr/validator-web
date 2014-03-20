092d5e91d34b701f6c1bdcea9653885aa197219f
sverosak <sveinung.rosaker@gmail.com>
2014-03-20 12:51:16 +0100

    Fix client build script

--- scripts/watch-browser-js.sh
+++ scripts/watch-browser-js.sh
@@ -2,4 +2,4 @@
 mkdir -p ./client/built &&
 ./node_modules/.bin/watchify -r domready -r dom-events -o client/built/common.js -dv &
 ./node_modules/.bin/watchify client/js/main.js -x domready -x dom-events  -o client/built/main.js -dv &
-./node_modules/.bin/watchify client/js/editor.js -x domready -x dom-events -o client/built/editor.js
+./node_modules/.bin/watchify client/js/editor.js -x domready -x dom-events -o client/built/editor.js -dv
