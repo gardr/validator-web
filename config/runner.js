fddd6900864edcc9aa88363ad2be25418990ec9b
sveisvei <sveinung.rosaker@gmail.com>
2014-06-25 15:18:33 +0200

    Run preview and gardr iframe on different domain. Closes #27

---
+++ config/runner.js
@@ -0,0 +1,29 @@
+var pack = require('../package.json');
+
+module.exports = function(conf) {
+
+    var base;
+
+    var version = 'w'+pack.version + '_' + 'v'+pack.dependencies['gardr-validator'] + 'h'+pack.dependencies['gardr-host'];
+
+
+    if (conf.get('env') === 'production'){
+
+        base = 'http://ads-gardr.gardr.org/';
+
+
+        return {
+            parentUrl: base+'/preview/parent.html?g='+version,
+            iframeUrl: base+'/preview/built/iframe.html?g='+version,
+            resourceDomainBase: base,
+        };
+    }
+
+    base = 'http://127.0.0.1:'+conf.get('port');
+    return {
+        parentUrl: base+'/preview/parent.html?ts='+Date.now()+'&g='+version,
+        iframeUrl: base+'/preview/built/iframe.html?ts='+Date.now()+'&g='+version,
+        resourceDomainBase: base
+    };
+
+}
