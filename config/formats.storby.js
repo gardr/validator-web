87f5a55b7484db4343e4cd9df375c681285edebc
Ole Sandring <sandring@outlook.com>
2015-01-09 09:35:31 +0100

    Merge Storby into one entry

--- config/formats.storby.js
+++ config/formats.storby.js
@@ -1,97 +1,58 @@
-var logo = "http://www.storbyinfo.no/wp-content/uploads/2013/11/logo.png";
 module.exports = [{
-        "name": "storby-mobil",
-        "title": "Storby Mobil",
-        "logo": logo,
-        "meta": [
-            "mobile"
-        ],
-        "config": {},
-        "formats": [{
-            "width": {
-                "min": 468,
-                "max": 468
-            },
-            "height": {
-                "min": 300,
-                "max": 300
+    'name': 'storby',
+    'title': 'Storby',
+    'logo': 'http://www.storbyinfo.no/wp-content/uploads/2013/11/logo.png',
+    'meta': ['mobile', 'tablet', 'desktop'],
+    'subFormats': {
+        'non-strict': {
+            config: {
+                timers: {
+                    setTimeout: 100
+                }
             }
-        }, {
-            "width": {
-                "min": 468,
-                "max": 468
-            },
-            "height": {
-                "min": 468,
-                "max": 468
-            }
-        }]
+        }
+    },
+    'formats': [{
+        'viewport': {
+            width: 980,
+            height: 150
+        },
+        'width': 980,
+        'height': 150
+    },{
+        'viewport': {
+            width: 980,
+            height: 300
+        },
+        'width': 980,
+        'height': 300
+    },{
+        'viewport': {
+            width: 980,
+            height: 500
+        },
+        'width': 980,
+        'height': 500,
     }, {
-        "name": "storby-desktop-tablet",
-        "title": "Storby Desktop/Tablet",
-        "logo": logo,
-        "meta": [
-            "tablet",
-            "desktop"
-
-        ],
-        "config": {},
-        "formats": [{
-                "width": {
-                    "min": 980,
-                    "max": 980
-                },
-                "height": {
-                    "min": 150,
-                    "max": 150
-                }
-            }, {
-                "width": {
-                    "min": 980,
-                    "max": 980
-                },
-                "height": {
-                    "min": 300,
-                    "max": 300
-                }
-            }, {
-                "width": {
-                    "min": 980,
-                    "max": 980
-                },
-                "height": {
-                    "min": 600,
-                    "max": 600
-                }
-            }, {
-                "width": {
-                    "min": 580,
-                    "max": 580
-                },
-                "height": {
-                    "min": 400,
-                    "max": 400
-                }
-            }, {
-                "width": {
-                    "min": 380,
-                    "max": 380
-                },
-                "height": {
-                    "min": 600,
-                    "max": 600
-                }
-            }, {
-                "width": {
-                    "min": 180,
-                    "max": 180
-                },
-                "height": {
-                    "min": 500,
-                    "max": 500
-                }
-            }
-
-        ]
-    }
-];
+        'viewport': {
+            width: 380,
+            height: 500
+        },
+        'width': 380,
+        'height': 500
+    }, {
+        'viewport': {
+            width: 180,
+            height: 500
+        },
+        'width': 180,
+        'height': 500
+    }, {
+        'viewport': {
+            width: 320,
+            height: 250
+        },
+        'width': 320,
+        'height': 250
+    }]
+}];
