5dbf24da87c6167fc6ad17695ad6a56c05105653
Ole Sandring <sandring@outlook.com>
2015-03-27 13:58:19 +0100

    Update to the new format 250px height

--- config/formats.js
+++ config/formats.js
@@ -2,22 +2,22 @@ module.exports = [{
     'name': 'inma',
     'title': 'Industry standard - HTML Responsive ads',
     'logo': 'http://www.brandsoftheworld.com/sites/default/files/styles/logo-thumbnail/public/092011/inma_green.gif',
-    'meta': ['mobile'],
+    'meta': ['mobile', 'tablet'],
     'config': {
 
     },
     'formats': [{
         'viewport': {
             width: 980,
-            height: 225
+            height: 250
         },
         'width': {
             min: 980,
             max: 980
         },
         'height': {
-            min: 225,
-            max: 225
+            min: 250,
+            max: 250
         }
     }]
 }];
