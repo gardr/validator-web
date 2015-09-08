1a65ab6e9e0764dbf74af588785bb90bcdf58582
sverosak <sveinung.rosaker@finn.no>
2015-09-08 10:41:53 +0200

    remove lint option

--- scripts/build-browser-js.sh
+++ scripts/build-browser-js.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mkdir -p ./client/built &&
 ./node_modules/.bin/browserify -r domready -r dom-events -o client/built/common.js &&
-./node_modules/.bin/uglifyjs client/built/common.js -mc --lint=false > client/built/common.min.js &
+./node_modules/.bin/uglifyjs client/built/common.js -mc > client/built/common.min.js &
 ./node_modules/.bin/browserify client/js/main.js -x domready -x dom-events  -o client/built/main.js &&
-./node_modules/.bin/uglifyjs client/built/main.js -mc --lint=false > client/built/main.min.js &
+./node_modules/.bin/uglifyjs client/built/main.js -mc > client/built/main.min.js &
 ./node_modules/.bin/browserify client/js/editor.js -x domready -x dom-events -o client/built/editor.js &&
-./node_modules/.bin/uglifyjs client/built/editor.js -mc --lint=false > client/built/editor.min.js &&
+./node_modules/.bin/uglifyjs client/built/editor.js -mc > client/built/editor.min.js &&
 echo "done"
