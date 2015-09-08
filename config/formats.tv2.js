1237fb352127f3589365e5e153367a6ff10aa4d8
sverosak <sveinung.rosaker@finn.no>
2015-09-08 10:53:17 +0200

    add tv2 formats, closes #62

---
+++ config/formats.tv2.js
@@ -0,0 +1,103 @@
+module.exports = [{
+    "name": "tv2",
+    "title": "TV 2",
+    "logo": "http://static.cdn.tv2.no/s/img/logo/tv2-logo-90.svg",
+    "meta": [
+        "mobile",
+        "tablet",
+        "desktop"
+    ],
+    "config": {
+        "sizes": {
+            "maxRequests": {
+                "style": 1
+            }
+        },
+        "touch": {
+           "swipeTop": false,
+        }
+    },
+    "formats": [
+        {
+            "description": "Toppbanner",
+            "viewport": {
+                "width": 1000,
+                "height": 300
+            },
+            "width": 1000,
+            "height": 300
+        },
+        {
+            "description": "Superboard",
+            "viewport": {
+                "width": 980,
+                "height": 150
+            },
+            "width": 980,
+            "height": 150
+        },
+        {
+            "description": "Superboard",
+            "viewport": {
+                "width": 980,
+                "height": 300
+            },
+            "width": 980,
+            "height": 300
+        },
+        {
+            "description": "Superboard",
+            "viewport": {
+                "width": 980,
+                "height": 600
+            },
+            "width": 980,
+            "height": 600
+        },
+        {
+            "description": "Skyskraper",
+            "viewport": {
+                "width": 300,
+                "height": 600
+            },
+            "width": 300,
+            "height": 600
+        },
+        {
+            "description": "Rektangel",
+            "viewport": {
+                "width": 300,
+                "height": 250
+            },
+            "width": 300,
+            "height": 250
+        },
+        {
+            "description": "Nettboard",
+            "viewport": {
+                "width": 468,
+                "height": 200
+            },
+            "width": 468,
+            "height": 200
+        },
+        {
+            "description": "Nettboard",
+            "viewport": {
+                "width": 468,
+                "height": 300
+            },
+            "width": 468,
+            "height": 300
+        },
+        {
+            "description": "Nettboard",
+            "viewport": {
+                "width": 468,
+                "height": 400
+            },
+            "width": 468,
+            "height": 400
+        }
+    ]
+}];
